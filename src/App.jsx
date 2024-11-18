d7fd4897a0d1f927c1fad113d531648abd5a328b
Jnyandeep <thokchom.jnyandeep@gmail.com>
2024-11-18 16:15:29 +0530

    setup: page setup

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import { BrowserRouter as Router, Route, Routes} from 'react-router-dom'
+import Home from './pages/Home'
+import Transaction from './pages/Transaction'
+
+
+const App = () => {
+  return (
+    // <Router>
+    //     <Route path='/' component={<Home />} />
+    //     <Route path='/transaction' component={<Transaction />} />
+    // </Router>
+    <div>
+        Hello
+    </div>
+  )
+}
+
+export default App
